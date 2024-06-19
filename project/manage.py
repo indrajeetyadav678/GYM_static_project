b3c82763154ee5a42ba0fcb68036a46e47649202
Indrajeet <yindrajeet678@gmail.com>
2024-06-20 03:01:21 +0530

    vercel deploys

--- project/manage.py
+++ project/manage.py
@@ -20,3 +20,14 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
+application = get_wsgi_application()
+
+if __name__ == "__main__":
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
